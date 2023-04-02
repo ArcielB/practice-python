09cab5f55a1b2945dca217273f16755ee75d932f
ArcielB <baezarciel@gmail.com>
2023-04-02 16:20:04 +0300

    qwr

--- recursive.py
+++ recursive.py
@@ -1,1 +1,8 @@
-print("recursive")
+def fibonacci(current, last, limit):
+    print(current)
+    last, current = current, current + last
+    if current <= limit:
+        fibonacci(current, last, limit)
+
+toprint = fibonacci(1, 0, int(input("what's the limit")))
+toprint
